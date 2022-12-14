76dee02133c168f10278475f57597ecf940f6ecd
Ossian Winter <ossian@hey.com>
2022-12-14 14:43:29 +0100

    chore: delete `copy-to-prod-env.sh` script

--- seppuku-plugins/copy-to-prod-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-for f in */build/libs/*.jar; do
-	echo "cp '$f' -> '~/.local/share/PrismLauncher/instances/1.19.3-fabric/.minecraft/seppuku/plugins/'"
-	cp "$f" ~/.local/share/PrismLauncher/instances/1.19.3-fabric/.minecraft/seppuku/plugins/
-done
