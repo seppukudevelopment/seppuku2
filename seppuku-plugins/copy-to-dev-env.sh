92f394ca6c93e1858b6fa8b3b058e9ca229e0da2
Ossian Winter <ossian@hey.com>
2022-12-14 14:51:08 +0100

    chore: improve `copy-to-dev-env.sh` script
    Now creates directory if it doesn't exist

--- seppuku-plugins/copy-to-dev-env.sh
+++ seppuku-plugins/copy-to-dev-env.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
+PLUGINS_PATH="../seppuku-client/run/seppuku/plugins/"
+
+if [ ! -d "$PLUGINS_PATH" ] ; then
+  echo "make dir '$PLUGINS_PATH'"
+  mkdir -p "$PLUGINS_PATH"
+fi
+
 for f in */build/devlibs/*.jar; do
-	echo "cp '$f' -> '../seppuku-client/run/seppuku/plugins/'"
-	cp "$f" ../seppuku-client/run/seppuku/plugins/
+	echo "copy '$f' to '$PLUGINS_PATH'"
+	cp "$f" "$PLUGINS_PATH"
 done
