d130066eb0e7b888704469a7eb76850246cc938a
Ossian Winter <ossian@hey.com>
2022-12-16 15:07:31 +0100

    fix: re-enable config persistence

--- seppuku-client/src/main/kotlin/pw/seppuku/client/ecs/entities/ConfigPersistenceEntity.kt
+++ seppuku-client/src/main/kotlin/pw/seppuku/client/ecs/entities/ConfigPersistenceEntity.kt
@@ -3,19 +3,25 @@ package pw.seppuku.client.ecs.entities
 import pw.seppuku.client.ecs.components.minecraft.client.MinecraftClientInitEarly
 import pw.seppuku.components.HumanIdentifier
 import pw.seppuku.ecs.Component
+import pw.seppuku.ecs.Engine
 import pw.seppuku.ecs.Entity
+import pw.seppuku.ecs.mapComponent
+import pw.seppuku.settings.config.Config
+import kotlin.concurrent.thread
 
-internal abstract class ConfigPersistenceEntity : Entity {
+internal abstract class ConfigPersistenceEntity(
+  private val engine: Engine
+) : Entity {
 
   @Component val humanIdentifier = HumanIdentifier("config_persistence")
 
   @Component val minecraftClientInitEarly = MinecraftClientInitEarly {
-    /*Seppuku.engine.mapComponent<Config>().filter(Config::exists).forEach {
+    engine.mapComponent<Config>().filter(Config::exists).forEach {
       it.readFromFile()
     }
 
     Runtime.getRuntime().addShutdownHook(thread(start = false) {
-      Seppuku.engine.mapComponent<Config>().forEach { it.writeToFile() }
-    })*/
+      engine.mapComponent<Config>().forEach { it.writeToFile() }
+    })
   }
 }
