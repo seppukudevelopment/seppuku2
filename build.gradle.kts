b4fedb10adf76233f040e98736818512d7dee8f4
Ossian Winter <ossian@hey.com>
2022-12-17 14:19:53 +0100

    chore: fully remove shadow plugin dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   alias(mainLibs.plugins.kotlin.jvm) apply false
-  alias(mainLibs.plugins.shadow) apply false
   alias(minecraftLibs.plugins.fabric.loom) apply false
   alias(testLibs.plugins.kotlinx.benchmark) apply false
 }
